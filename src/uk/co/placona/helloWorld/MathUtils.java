0426d1baefd09caf6cdbefe2e3b2ff4503128afe
Peter Yu <wb-yp393743@alibaba-inc.com>
2019-07-24 10:30:13 +0800

    更新build模板

---
+++ src/uk/co/placona/helloWorld/MathUtils.java
@@ -0,0 +1,15 @@
+package uk.co.placona.helloWorld;
+
+/**
+ * @author Peter Yu 2019/7/23 17:19
+ */
+public class MathUtils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+}
---
+++ test/uk/co/placona/helloWorld/MathUtilsTest.java
@@ -0,0 +1,20 @@
+package uk.co.placona.helloWorld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Peter Yu 2019/7/24 9:36
+ */
+public class MathUtilsTest {
+
+    @Test
+    public void add() {
+        assertEquals(3, MathUtils.add(1, 2));
+    }
+
+    @Test
+    public void minus() {
+    }
+}
